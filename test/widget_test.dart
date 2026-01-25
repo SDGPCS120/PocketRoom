28f220686f3c7f128a3ebca3e3cf3e345637acb8
Manula Thejan <thejanmanula@gmail.com>
2026-01-25 11:55:23 +0530

    add testing for current widgets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,22 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:pocketroom/main.dart';
+import 'package:pocketroom/src/common_widgets/search_bar_widget.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Renders HomePage and finds search bar', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // We must wrap the app in a ProviderScope for Riverpod to work in tests.
+    await tester.pumpWidget(const ProviderScope(
+      child: PocketRoomApp(),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that a key widget, like the search bar, is present.
+    expect(find.byType(SearchBarWidget), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // You could also verify text within the search bar.
+    expect(find.widgetWithText(TextField, 'Search'), findsOneWidget);
   });
 }
