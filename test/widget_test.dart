d401b6eaf34f0d0819661311d7dfb04ab478126c
Manula Thejan <thejanmanula@gmail.com>
2026-01-29 00:22:56 +0530

    turns out a blank test file still fails, now there's a 'always pass' test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,8 @@
-//blank for now
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('blank test that always passes', () {
+    // This test does nothing, it's supposed to pass(made to figure out the ci)
+    expect(true, isTrue);
+  });
+}
