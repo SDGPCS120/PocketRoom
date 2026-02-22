8802c5911a89c94cf995e3914c9dd25ff5ab12ee
chanduligunawardena <chanduligunawardena@gmail.com>
2026-02-22 20:59:18 +0530

    A different folder was made for enums

--- backend/src/modules/room-member/dto/update-room-member.dto.ts
+++ backend/src/modules/room-member/dto/update-room-member.dto.ts
@@ -1,5 +1,5 @@
 import { IsString, IsEnum } from 'class-validator';
-import { RoomRole } from './room-role.enum';
+import { RoomRole } from '../enums/room-role.enum';
 
 export class UpdateRoomMemberRoleDto {
 
