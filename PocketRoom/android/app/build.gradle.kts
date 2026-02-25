4e67a567500ea9c34a9b755961a39bcebc706677
Prageesha-23 <prageeshaprabhager@gmail.com>
2026-02-25 12:11:52 +0530

    AI search

--- PocketRoom/android/app/build.gradle.kts
+++ PocketRoom/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.pocketroom"
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
