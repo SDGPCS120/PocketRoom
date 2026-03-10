69bf8e327a348fdb9aedb4ba5661af78d40c1aff
chanduligunawardena <chanduligunawardena@gmail.com>
2026-03-10 14:30:11 +0530

    Update .gitignore and category update DTO

--- backend/src/modules/category/dto/update-category.dto.ts
+++ backend/src/modules/category/dto/update-category.dto.ts
@@ -1,29 +1,4 @@
-import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCategoryDto } from './create-category.dto';
 
-export class UpdateCategoryDto {
-
-  @IsOptional()
-  @IsString()
-  name?: string;
-
-  @IsOptional()
-  @IsString()
-  slug?: string;
-
-  @IsOptional()
-  @IsString()
-  description?: string;
-
-  @IsOptional()
-  @IsString()
-  parentCategoryId?: string;
-
-  @IsOptional()
-  @IsString()
-  imageURL?: string;
-
-  @IsOptional()
-  @IsBoolean()
-  isActive?: boolean;
-
-}
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
