7edaf584d18062c61ba7843284de6d7a12afba37
chanduligunawardena <chanduligunawardena@gmail.com>
2026-03-10 14:14:11 +0530

    Update category DTOs

--- backend/src/modules/category/dto/create-category.dto.ts
+++ backend/src/modules/category/dto/create-category.dto.ts
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
-=======
-import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from 'class-validator';
->>>>>>> bf9be906f4f4cfb86db39e920f6ebce8aee49a77
+
+import {IsBoolean, IsInt, IsNotEmpty, IsOptional, IsString, Min } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty()
-<<<<<<< HEAD
   name: string;
 
   @IsString()
@@ -17,9 +13,6 @@ export class CreateCategoryDto {
   @IsOptional()
   @IsString()
   description?: string;
-=======
-  categoryName: string;
->>>>>>> bf9be906f4f4cfb86db39e920f6ebce8aee49a77
 
   @IsOptional()
   @IsString()
@@ -27,19 +20,9 @@ export class CreateCategoryDto {
 
   @IsOptional()
   @IsString()
-<<<<<<< HEAD
   imageURL?: string;
 
   @IsOptional()
   @IsBoolean()
   isActive?: boolean;
 }
-=======
-  categoryDescription?: string;
-
-  @IsOptional()
-  @IsInt()
-  @Min(0)
-  displayOrder?: number;
-}
->>>>>>> bf9be906f4f4cfb86db39e920f6ebce8aee49a77
