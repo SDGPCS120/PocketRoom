921efab2f2264a78df2412ace9cfe69dc8280a50
Gamersooreal <lehanli06@gmail.com>
2026-03-18 20:55:54 +0530

    Merged budgeting feature with Dev

--- frontend/mobile/android/app/build.gradle.kts
+++ frontend/mobile/android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.pocketroom"
     compileSdk = 36
-    ndkVersion = "29.0.14206865"
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -27,7 +27,8 @@ android {
         applicationId = "com.example.pocketroom"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // minSdk = flutter.minSdkVersion
+        minSdk = 30
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
