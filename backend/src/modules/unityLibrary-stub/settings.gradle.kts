8cbc29036b46cd41359d26bbfe0738ed47680c50
Gamersooreal <lehanli06@gmail.com>
2026-03-19 08:48:20 +0530

    Add standalone Gradle settings for unityLibrary stub

---
+++ backend/src/modules/unityLibrary-stub/settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    plugins {
+        id("com.android.library") version "8.9.1"
+        id("org.jetbrains.kotlin.android") version "2.1.0"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "unityLibrary-stub"
