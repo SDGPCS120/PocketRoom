b846836642ab9a7a7c9856a8c7242df622780a49
Prageesha-23 <prageeshaprabhager@gmail.com>
2026-03-20 18:11:41 +0530

    splash organized

---
+++ frontend/mobile/lib/src/features/splash/presentation/widgets/animated_splash_logo.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class AnimatedSplashLogo extends StatefulWidget {
+  final VoidCallback onAnimationComplete;
+
+  const AnimatedSplashLogo({super.key, required this.onAnimationComplete});
+
+  @override
+  State<AnimatedSplashLogo> createState() => _AnimatedSplashLogoState();
+}
+
+class _AnimatedSplashLogoState extends State<AnimatedSplashLogo>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 0.5,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.elasticOut));
+
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.5, curve: Curves.easeIn),
+      ),
+    );
+
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        widget.onAnimationComplete();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Opacity(
+          opacity: _opacityAnimation.value,
+          child: Transform.scale(
+            scale: _scaleAnimation.value,
+            child: child,
+          ),
+        );
+      },
+      child: Image.asset(
+        'assets/logo.png',
+        width: 150,
+        height: 150,
+      ),
+    );
+  }
+}
