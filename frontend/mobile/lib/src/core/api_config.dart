ee113e41f8095db87fef4032ee971eb2dab47f37
StephanMendis001 <stephan.mendis04@gmail.com>
2026-03-22 12:09:47 +0530

    unify Firebase service for cloud compatibility

--- frontend/mobile/lib/src/core/api_config.dart
+++ frontend/mobile/lib/src/core/api_config.dart
@@ -13,5 +13,5 @@ class ApiConfig {
   // static const String baseUrl = 'http://10.0.2.2:8000';
 
   /// ALTERNATIVE: Local IP for physical mobile phone (must be on same Wi-Fi)
-  static const String baseUrl = 'http://192.168.1.6:8000';
+  static const String baseUrl = 'https://pocketroom-backend-93470454666.asia-south1.run.app';
 }
